ab8c0f02532444def52c6d2c69abe8bc9b9fcf2d
Pedro Augusto <65167194+unknowedguy00156@users.noreply.github.com>
2026-03-25 20:38:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Só o Instagram agora
+const insta = "https://www.instagram.com/useliriumjoias/";
+
+// Links
+document.getElementById("instagramLink").href = insta;
+document.getElementById("instaTop").href = insta;
